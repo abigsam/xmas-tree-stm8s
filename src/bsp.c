7c2c1a0cb8bf39e60b89a6761423bee9d62df13d
abigsam <abigsam@gmail.com>
2019-12-09 22:56:35 +0200

    Add template for BSP c/h files; add them to the IAR and STVD

---
+++ src/bsp.c
@@ -0,0 +1,18 @@
+/**
+ * @file bsp.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-12-09
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+#include "bsp.h"
+
+
+
+
+
+
---
+++ src/bsp.h
@@ -0,0 +1,24 @@
+/**
+ * @file bsp.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-12-09
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+
+#ifndef __BSP_H
+#define __BSP_H
+
+#include "stm8s.h"
+#include "pt.h"
+
+
+
+
+
+
+#endif
