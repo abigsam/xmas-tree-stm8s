85b5df4d0a1f17c5280214aead24d68ebbe6f6e3
abigsam <abigsam@gmail.com>
2019-12-10 00:32:44 +0200

    Add gpio.h with GPIO defines and default macroses

---
+++ src/gpio.h
@@ -0,0 +1,50 @@
+/**
+ * @file gpio.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-12-09
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
+
+#ifndef __GPIO_H
+#define __GPIO_H
+
+
+//GPIO defines
+#define GREEN_LEDS_PORT     (GPIOC)
+#define GLEDS_GPIO_0        (GPIO_PIN_3)
+#define GLEDS_GPIO_1        (GPIO_PIN_4)
+#define GLEDS_GPIO_2        (GPIO_PIN_5)
+#define GLEDS_GPIO_3        (GPIO_PIN_6)
+#define GLEDS_GPIO_4        (GPIO_PIN_7)
+//RGB LEDs
+#define RGB_LEDS_PORT       (GPIOD)
+#define RGB_LEDS_PIN        (GPIO_PIN_3)
+#define RGB_LEDS_PWR_PORT   (GPIOB)
+#define RGB_LEDS_PWR_PIN    (GPIO_PIN_5)
+//Light sensor
+#define LS_POWER_EN_PORT    (GPIOB)
+#define LS_POWER_EN_PIN     (GPIO_PIN_4)
+#define LS_OUTPUT_PORT      (GPIOD)
+#define LS_OUTPUT_PIN       (GPIO_PIN_2)
+#define LS_ADC_CHANNEL      (ADC1_CHANNEL_3)
+#define LS_SCHMIT_TRIGGER   (ADC1_SCHMITTTRIG_CHANNEL3)
+//Button
+#define BUTTON_PORT         (GPIOD)
+#define BUTTON_PIN          (GPIO_PIN_6)
+//Power managment
+#define BOOST_EN_PORT       (GPIOD)
+#define BOOST_EN_PIN        (GPIO_PIN_5)
+
+//Macroses
+#define GPIO_GET(port,pin)              ( port->IDR & (uint8_t)pin )
+#define GPIO_SET(port,pin)              { port->ODR |= (uint8_t)pin; }
+#define GPIO_RESET(port,pin)            { port->ODR &= ~(uint8_t)pin; }
+#define GPIO_CONTROL(port,pin,state)    { if (state) GPIO_SET(port,pin); \
+                                          else GPIO_RESET(port,pin); }
+
+#endif //__GPIO_H
